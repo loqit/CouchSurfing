54597676e280da1556c1c17755c7d22d336f1ea7
loqit <bobrandrey1972@gmail.com>
2021-10-17 02:45:10 +0300

    Add basic realization of tab bar

--- CouchSurfing/SceneDelegate.swift
+++ CouchSurfing/SceneDelegate.swift
@@ -10,9 +10,31 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    private var uniqueTag = UniqueTag()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = createTabBarController()
+    }
+    
+    private func createController(_ viewController: UIViewController,
+                                  title: String = "",
+                                  barItem: UITabBarItem.SystemItem) -> UINavigationController {
+        viewController.title = title
+        let tag = uniqueTag.getUniqueTag()
+        viewController.tabBarItem = UITabBarItem(tabBarSystemItem: barItem, tag: tag)
+        return UINavigationController(rootViewController: viewController)
+    }
+    
+    private func createTabBarController() -> UITabBarController {
+        let tabBar = UITabBarController()
+        UITabBar.appearance().tintColor = .black
+        let mainVC = createController(MainViewController(), title: "Home", barItem: .search)
+        tabBar.viewControllers = [mainVC]
+        
+        return tabBar
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
